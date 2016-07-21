ef5fd4c432bc1f8351dfce079db66d8a06c9298b
Grant Mercer <Gmercer015@gmail.com>
2016-07-21 16:44:43 -0700

    Finish subcategory

--- js/controllers/AddItemController.js
+++ js/controllers/AddItemController.js
@@ -1,30 +1,5 @@
-app.controller('AddItemController', ['$scope', '$filter', 'products', '$routeParams', function($scope, $filter, products, $routeParams) {
-    if($routeParams.category != null) {
-        if($routeParams.subcategory == null) {
-            $('#dropdown-subcategory').show();
-            $scope.filters = {category : $routeParams.category}
-        } else {
-            $scope.filters = {
-                category : $routeParams.category,
-                subcategory: $routeParams.subcategory
-            };
-            /*
-            for(var i = 0; i < $scope.api_categories.length; i++) {
-                if($scope.api_categories[i].name === $routeParams.category) {
-                    $scope.category = $scope.api_categories[i].name;
-                    $scope.subcategories = $scope.api_categories[i].subcategories;
-                    $scope.pretty_subcategories = $scope.pretty_categories[i];
-                }
-            }
-            */
-        }
-    }
-
-    products.success(function(data) {
-        $scope.lproducts = data;
-        $scope.lproducts = $filter('filter')($scope.lproducts, $scope.filters);
-        $scope.lproducts.loaded = true;
-    });
+app.controller('AddItemController', ['$scope', '$filter', 'products', '$routeParams',
+function($scope, $filter, products, $routeParams) {
 
     // hard coded catagories for the add item page
     $scope.api_categories = [
@@ -66,4 +41,36 @@ app.controller('AddItemController', ['$scope', '$filter', 'products', '$routePar
             }
         );
     });
+
+    // if the controller was entered with a route (e.g. #/additem/HARDWARE)
+    if($routeParams.category != null) {
+        // show the subcategory menu
+        $('#dropdown-subcategory').show();
+        if($routeParams.subcategory == null) {
+            // if no subcategory was selected, only filter by category
+            $scope.filters = {category : $routeParams.category}
+        } else {
+            // filter by both category and subcategory
+            $scope.filters = {
+                category : $routeParams.category,
+                subcategory: $routeParams.subcategory
+            };
+        }
+        // find the current category and set the vars used to populate the
+        // subcategory dropdown menu
+        for(var i = 0; i < $scope.api_categories.length; i++) {
+            if($scope.api_categories[i].name === $routeParams.category) {
+                $scope.currentCategory = $scope.api_categories[i];
+                $scope.prettyCurrentCategories = $scope.pretty_categories[i];
+            }
+        }
+    }
+
+    // load data to be stored, and filter data based upon the filters passed
+    products.success(function(data) {
+        $scope.lproducts = data;
+        $scope.lproducts = $filter('filter')($scope.lproducts, $scope.filters);
+    });
+
+
 }]);
