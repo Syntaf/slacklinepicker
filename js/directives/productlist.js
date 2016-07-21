7f2e9d9af595c2ec68d33ce14b2d74d063257949
Grant Mercer <Gmercer015@gmail.com>
2016-07-21 11:00:59 -0700

    write description populator

--- js/directives/productlist.js
+++ js/directives/productlist.js
@@ -5,6 +5,14 @@ app.directive('productlist', function() {
             info: '=',
             filters: '='
         },
-        templateUrl: 'js/directives/productlist.html'
+        templateUrl: 'js/directives/productlist.html',
+        link: function(scope, element, attrs) {
+            scope.fillDescription = function(product) {
+                $('#description-box').text(product.description);
+                $('#description-box').append(
+                    '<a target="_blank" href=' + product.link +
+                    ' id="product-link">Product Page</a>');
+            }
+        }
     };
 });
