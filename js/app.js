2a6d9872fbfacc83e344f909e985d17c2cf84824
Grant Mercer <Gmercer015@gmail.com>
2016-07-21 23:06:02 -0700

    make main.html pretty, refactor filter

--- js/app.js
+++ js/app.js
@@ -27,6 +27,6 @@ app.value('currentKit', []);
 
 app.filter('capitalize', function() {
     return function(input) {
-        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+        return input.replace('_', ' ').replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     }
 });
