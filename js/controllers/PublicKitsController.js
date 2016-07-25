573eaa3dc466d795b8a2be56949a24a2718fbc9a
Grant Mercer <Gmercer015@gmail.com>
2016-07-25 16:48:23 -0700

    write public page displaying

--- js/controllers/PublicKitsController.js
+++ js/controllers/PublicKitsController.js
@@ -1,3 +1,20 @@
-app.controller('PublicKitsController', ['$scope', function($scope) {
+app.controller('PublicKitsController', ['$scope', 'builds', function($scope, builds) {
+    // needed for configuring the database scrollbar
+    $scope.scrollbarConfig = {
+        autoHideScrollbar: false,
+        theme: 'light',
+        mouseWheel: {
+            scrollAmount: 300
+        },
+        advanced:{
+            updateOnContentResize: true
+        },
+        scrollInertia: 900
+    };
+
+    builds.success(function(data) {
+        $scope.buildList = data;
+        $scope.doneLoad = true;
+    });
 
 }]);
