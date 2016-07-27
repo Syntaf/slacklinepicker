6260a77cba4dbb55169d54efc59d7fce9cd995ff
Grant Mercer <Gmercer015@gmail.com>
2016-07-26 22:12:21 -0700

    Update products.js

--- js/services/products.js
+++ js/services/products.js
@@ -1,6 +1,6 @@
 app.factory('products', ['$http', function($http) {
         return $http.get(
-            'https://gist.githubusercontent.com/Syntaf/4c6704cbf128228ff50bdb2fd22c825c/raw/22ca8b5f75866adb3e7f7809b81d514541558e75/slacklineproducts.json')
+            'https://gist.githubusercontent.com/Syntaf/4c6704cbf128228ff50bdb2fd22c825c/raw/2315437999e95af2161e3b38b0721fc77299aea9/slacklineproducts.json')
             .success(function(data) {
                 return data;
             })
