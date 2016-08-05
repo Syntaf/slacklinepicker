e2eca850626bbc701b800b87ec42a4bdcbb1c618
Grant Mercer <Gmercer015@gmail.com>
2016-08-05 12:33:47 -0700

    comment out api

--- js/services/builds.js
+++ js/services/builds.js
@@ -1,6 +1,6 @@
 app.factory('builds', ['$http', function($http) {
     return $http.get(
-        'https://sheetsu.com/apis/v1.0/d9acf6c52e0b/sheets/BuildsAPI')
+        '')//'https://sheetsu.com/apis/v1.0/d9acf6c52e0b/sheets/BuildsAPI')
         .success(function(data) {
             return data;
         })
